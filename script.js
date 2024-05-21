a9947aefa2f4217311f7e136ca2b52916a1d9c25
BomberQLF <tmurphyparis@gmail.com>
2024-05-21 16:07:25 +0200

    Sauvegarde 21/05

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('mail.json')
-      .then(function (response) {
-          return response.json();
-      })
-      .then(function (data) {
-          console.log(data);
-          const domainesTemporaires = data.domainesTemporaires;
-
-          const emailInput = document.getElementById('email');
-
-          emailInput.addEventListener('input', function () {
-              const emailValue = emailInput.value;
-              const domaine = emailValue.split('@')[1];
-
-              if (domaine && domainesTemporaires.includes(domaine)) {
-                  emailInput.style.backgroundColor = 'red';
-              } else {
-                  emailInput.style.backgroundColor = '';
-              }
-          });
-      })
-      .catch(function (error) {
-          console.error('Erreur en fetchant les données JSON:', error);
-      });
-
-  var menuNav = document.querySelectorAll(".menuNav");
-
-  menuNav.forEach(function (navBarName) {
-      navBarName.addEventListener("mouseover", function () {
-          this.style.transition = "color 0.5s ease";
-          this.style.color = "#29d9d5";
-      });
-
-      navBarName.addEventListener("mouseout", function () {
-          this.style.transition = "color 0.5s ease";
-          this.style.color = "";
-      });
-  });
-
-  // CSS
-  var elementsDecouvrir = document.querySelectorAll(".boxDecouvrir");
-
-  elementsDecouvrir.forEach(function (element) {
-      element.addEventListener("mouseover", function (event) {
-          event.target.style.textDecoration = "underline";
-      });
-
-      element.addEventListener("mouseout", function (event) {
-          event.target.style.textDecoration = "none";
-      });
-  });
-
-  // CALCUL DU PRIX TOTAL DE LA RESERVATION DE MON FORMULAIRE
+    fetch('mail.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            const domainesTemporaires = data.domainesTemporaires;
+  
+            const emailInput = document.getElementById('email');
+  
+            emailInput.addEventListener('input', function () {
+                const emailValue = emailInput.value;
+                const domaine = emailValue.split('@')[1];
+  
+                if (domaine && domainesTemporaires.includes(domaine)) {
+                    emailInput.style.backgroundColor = 'red';
+                } else {
+                    emailInput.style.backgroundColor = '';
+                }
+            });
+        })
+        .catch(function (error) {
+            console.error('Erreur en fetchant les données JSON:', error);
+        });
+  
+    var menuNav = document.querySelectorAll(".menuNav");
+  
+    menuNav.forEach(function (navBarName) {
+        navBarName.addEventListener("mouseover", function () {
+            this.style.transition = "color 0.5s ease";
+            this.style.color = "#29d9d5";
+        });
+  
+        navBarName.addEventListener("mouseout", function () {
+            this.style.transition = "color 0.5s ease";
+            this.style.color = "";
+        });
+    });
+  
+    // CSS
+    var elementsDecouvrir = document.querySelectorAll(".boxDecouvrir");
+  
+    elementsDecouvrir.forEach(function (element) {
+        element.addEventListener("mouseover", function (event) {
+            event.target.style.textDecoration = "underline";
+        });
+  
+        element.addEventListener("mouseout", function (event) {
+            event.target.style.textDecoration = "none";
+        });
+    });
+  
+    // CALCUL DU PRIX TOTAL DE LA RESERVATION DE MON FORMULAIRE
   var yachtPrices = {
       1: 56000,
       2: 86000,
@@ -60,23 +60,24 @@ document.addEventListener("DOMContentLoaded", function () {
       5: 75000,
       6: 38000,
   };
-
+  
   var yachtSelect = document.getElementById("modele");
   var dateDebutInput = document.getElementById("date_debut");
   var dateFinInput = document.getElementById("date_fin");
   var prixTotalDiv = document.getElementById("prix-total");
-
+  var prixTotalInput = document.getElementById("prix_total_hidden");
+  
   var secondModeleSelect = document.getElementById("second_modele");
   var secondDateDebutInput = document.getElementById("second_date_debut");
   var secondDateFinInput = document.getElementById("second_date_fin");
-
+  
   secondDateDebutInput.style.display = "none";
   secondDateFinInput.style.display = "none";
-
+  
   yachtSelect.addEventListener("change", calculatePrice);
   dateDebutInput.addEventListener("change", calculatePrice);
   dateFinInput.addEventListener("change", calculatePrice);
-
+  
   secondModeleSelect.addEventListener("change", function () {
       if (secondModeleSelect.value !== "") {
           secondDateDebutInput.style.display = "block";
@@ -88,45 +89,46 @@ document.addEventListener("DOMContentLoaded", function () {
           calculatePrice();
       }
   });
-
+  
   secondDateDebutInput.addEventListener("change", calculatePrice);
   secondDateFinInput.addEventListener("change", calculatePrice);
-
+  
   function calculatePrice() {
       var yachtId = yachtSelect.value;
       var dateDebut = new Date(dateDebutInput.value);
       var dateFin = new Date(dateFinInput.value);
-
       var diffTime = Math.abs(dateFin - dateDebut);
-      const diffDays = Math.ceil(diffTime / 86400000); // Ca revient à faire le calcul suivant : 1000 * 60 * 60 * 24
-
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Conversion en jours
+  
       var yachtPrice = yachtPrices[yachtId];
-
       var price = diffDays * yachtPrice;
-
+  
+      // Conversion du prix en entier avant de l'afficher et de l'envoyer
+      price = parseInt(price, 10);
+      prixTotalInput.value = price;
       prixTotalDiv.textContent = `Prix total : ${price} €`;
-
-      var prixTotalInput = document.getElementById("prix_total_hidden");
-      // C'est un modèle ternaire utilisé aussi en PHP pour dire que si la valeur séléctionné dans le champ du second modele est vide alors total price, else price.
-      prixTotalInput.value = secondModeleSelect.value !== "" ? totalPrice : price;
-
+  
       if (secondModeleSelect.value !== "") {
           var secondYachtId = secondModeleSelect.value;
           var secondDateDebut = new Date(secondDateDebutInput.value);
           var secondDateFin = new Date(secondDateFinInput.value);
-
+  
           var secondDiffTime = Math.abs(secondDateFin - secondDateDebut);
           var secondDiffDays = Math.ceil(secondDiffTime / (1000 * 60 * 60 * 24));
-
+  
           var secondYachtPrice = yachtPrices[secondYachtId];
-
-          var totalPrice = price + secondDiffDays * secondYachtPrice;
-
+          var secondPrice = secondDiffDays * secondYachtPrice;
+          var totalPrice = price + secondPrice;
+  
           if (yachtId === secondYachtId) {
               prixTotalDiv.textContent = "Erreur : Vous ne pouvez pas réserver le même bateau pour les deux options.";
           } else {
+              // Conversion du totalPrice en entier avant de l'afficher et de l'envoyer
+              totalPrice = parseInt(totalPrice, 10);
+              prixTotalInput.value = totalPrice;
               prixTotalDiv.textContent = `Prix total : ${totalPrice} €`;
           }
       }
   }
-});
+  });
+  
--- traitement.php
+++ traitement.php
@@ -4,20 +4,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Confirmation de la réservation</title>
+    <style>
+        body {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            background-color: #222;
+        }
+        h1 {
+            color: #29d9d5;
+            font-size: 3rem;
+        }
+        p {
+            color: white;
+        }
+        a {
+            color: navajowhite;
+        }
+    </style>
 </head>
 
 <body>
     <?php
     // var_dump($_POST); exit;
     if (isset($_POST['reserver'])) {
-        $prix_total = $_POST['prix_total'];
+        // Intval pour convertir le prix total en un nombre entier et non une chaine de caractère
+        $prix_total = intval($_POST['prix_total']); 
+    
         // Connexion à la base de données
         $db = new PDO('mysql:host=localhost;dbname=sae_resa_web;port=8889', 'root', 'root');
 
         // Prépare la requête d'insertion
         $stmt = $db->prepare("INSERT INTO sae_reservation_client (id_bateaux, date_reservation, date_debut, date_fin, prix_total, nom, prenom, email, modele, second_modele, second_date_debut, second_date_fin) VALUES (:id_bateaux, NOW(), :date_debut, :date_fin, :prix_total, :nom, :prenom, :email, :modele, :second_modele, :second_date_debut, :second_date_fin)");
 
-        // Lie les paramètres
+        // Liaison des paramètres
         $stmt->bindParam(':id_bateaux', $_POST['modele']);
         $stmt->bindParam(':date_debut', $_POST['date_debut']);
         $stmt->bindParam(':date_fin', $_POST['date_fin']);
@@ -48,8 +70,7 @@
 
 
     <h1>Votre réservation a été enregistrée avec succès</h1>
-    <p>Merci pour votre réservation. Nous vous contacterons prochainement pour confirmer les détails de votre
-        réservation.</p>
+    <p>Merci pour votre réservation. Veuillez vérifier votre boite mail afin de visionner le récapitulatif de votre reservation.</p>
     <a href="index.php">Retour à la page d'accueil</a>
 </body>
 
